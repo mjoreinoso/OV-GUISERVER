152896efd09ae423a8cecca14a038a9aebc5acd6
mjoreinoso <mjoreinoso@gmail.com>
2025-07-18 16:28:43 -0500

    no graph

--- src/modules/RLD/components/IR_RLD/IR_RLD.vue
+++ src/modules/RLD/components/IR_RLD/IR_RLD.vue
@@ -8,7 +8,6 @@
       <LED_Strobe_Timing />
     </div>
     <div>
-      <Graph />
     </div>
   </div>
 
@@ -20,7 +19,6 @@
 
 <script setup lang="ts">
 import { onMounted, watch } from 'vue';
-import Graph from './components/graph.vue';
 import IR from './components/IR.vue';
 import LED_Strobe_Timing from './components/LED_Strobe_Timing.vue';
 import LightLevels from './components/lightLevels.vue';
