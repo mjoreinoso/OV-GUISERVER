c18c45f464e2b874821c787e85b02c4e23769f4d
HcapdevillaEf <hcapdevilla@efrouting.com>
2025-08-05 19:54:13 -0500

    Hacer un icono para los cambios de la botella

--- src/modules/OSW/masks/components/camera/liveFeedControl.vue
+++ src/modules/OSW/masks/components/camera/liveFeedControl.vue
@@ -1,21 +1,24 @@
 <template>
-    <div class="rounded-xl flex items-center justify-center space-x-3">
-        <div class="w-16 h-16 bg-secondary rounded-xl flex items-center justify-center">
+    <div class="flex items-center justify-center space-x-3 rounded-xl">
+        <div class="flex items-center justify-center w-16 h-16 bg-secondary rounded-xl">
             <img :src="bottleIcon" class="scale-75" alt="Bottle Icon">
         </div>
-        <div class="w-16 h-16 bg-secondary rounded-xl flex items-center justify-center">
+        <div class="flex items-center justify-center w-16 h-16 bg-secondary rounded-xl">
             <img :src="resolutionIcon" class="scale-75" alt="Resolution Icon">
         </div>
-        <div class="w-16 h-16 bg-secondary rounded-xl flex items-center justify-center">
-            3
+        <div class="flex flex-col items-center justify-center w-16 h-16 cursor-pointer bg-secondary rounded-xl"
+            @click="updateBottleCount">
+            <img :src="bottleImage" class="transition duration-300 scale-75" alt="Bottle Icon" />
+            <span class="mt-1 font-semibold">{{ bottleCount }}</span>
         </div>
-        <div class="w-16 h-16 bg-secondary rounded-xl flex items-center justify-center">
+        <div class="flex items-center justify-center w-16 h-16 bg-secondary rounded-xl">
             <img :src="freezeIcon" class="scale-75" alt="Freeze Icon">
         </div>
-        <div class="w-16 h-16 font-semibold text-[1.6vw] bg-secondary rounded-xl flex items-center justify-center cursor-pointer" @click="updateSeconds">
+        <div class="w-16 h-16 font-semibold text-[1.6vw] bg-secondary rounded-xl flex items-center justify-center cursor-pointer"
+            @click="updateSeconds">
             {{ seconds }}
         </div>
-        <div class="w-16 h-16 bg-secondary rounded-xl flex items-center justify-center">
+        <div class="flex items-center justify-center w-16 h-16 bg-secondary rounded-xl">
             <img :src="saveIcon" class="scale-75" alt="Save Icon">
         </div>
     </div>
@@ -27,8 +30,18 @@ import bottleIcon from '../../../../../assets/screenIcons/liveFeedIcons/bottle.s
 import resolutionIcon from '../../../../../assets/screenIcons/liveFeedIcons/resolution.svg';
 import saveIcon from '../../../../../assets/screenIcons/liveFeedIcons/save.svg';
 import freezeIcon from '../../../../../assets/screenIcons/liveFeedIcons/freeze.svg';
+import GroupBottle from '../../../../../assets/screenIcons/liveFeedIcons/groupBottle.svg';
+import GroupBottle1 from '../../../../../assets/screenIcons/liveFeedIcons/groupBottle_fr1.svg';
+import GroupBottle2 from '../../../../../assets/screenIcons/liveFeedIcons/groupBottle_fr2.svg';
+import GroupBottle3 from '../../../../../assets/screenIcons/liveFeedIcons/groupBottle_fr3.svg';
+import GroupBottle4 from '../../../../../assets/screenIcons/liveFeedIcons/groupBottle_fr4.svg';
 
 const seconds = ref(0);
+const bottleCount = ref(1);
+
+function updateBottleCount() {
+    bottleCount.value = bottleCount.value < 4 ? bottleCount.value + 1 : 1;
+}
 
 function updateSeconds() {
     seconds.value += 10;
@@ -36,5 +49,26 @@ function updateSeconds() {
         seconds.value = 0;
     }
 }
+
+import { computed as vueComputed } from 'vue';
+
+function computed(getter: () => string) {
+    return vueComputed(getter);
+}
+
+const bottleImage = computed(() => {
+    switch (bottleCount.value) {
+        case 1:
+            return GroupBottle1;
+        case 2:
+            return GroupBottle2;
+        case 3:
+            return GroupBottle3;
+        case 4:
+            return GroupBottle4;
+        default:
+            return GroupBottle1;
+    }
+});
 
 </script>
