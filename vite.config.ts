f176a76d21d59408f772fe9a1d092f3d3d896727
HenryCapdevilla <henrydaniel2002@hotmail.com>
2025-08-14 15:04:09 -0500

    Se corrigio el alias @ en vite.config.ts para resolver rutas correctamente

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/OV-GUISERVER/',
-  plugins: [vue(), tailwindcss(),],
+  plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
-      '@': '/src',
+      '@': path.resolve(__dirname, './src'),
     }
   },
 })
