3b44bd125f1fd8e91102864b5e3b4498f3a17382
HenryCapdevilla <henrydaniel2002@hotmail.com>
2025-09-03 08:13:12 -0500

    Se eliminaron archivos innecesarios

--- src/modules/CONTROL/components/FINISH/components/graph.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div class="flex items-center justify-center p-2 border-2 rounded-xl w-[55%]">
-      <div class="h-[70vh] w-[45vw]"><chartGraph /></div>
-    </div>
-</template>
-
-<script setup lang="ts">
-import chartGraph from './chartGraph.vue';
-</script>
